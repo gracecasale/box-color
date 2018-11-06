b7e8913d5bc237148fa9d332cdb146b73c703015
Grace Casale <gracecasale@gmail.com>
2018-11-05 20:08:31 -0500

    Updates

---
+++ precache-manifest.8d384d9522ac8bdc0ef7851233dc25df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "997c74cf7251adf80e3a",
+    "url": "/box-color/static/js/runtime~main.997c74cf.js"
+  },
+  {
+    "revision": "be86226760a25504dda5",
+    "url": "/box-color/static/js/main.be862267.chunk.js"
+  },
+  {
+    "revision": "e4030dac74914b555392",
+    "url": "/box-color/static/js/1.e4030dac.chunk.js"
+  },
+  {
+    "revision": "be86226760a25504dda5",
+    "url": "/box-color/static/css/main.aa2e83e1.chunk.css"
+  },
+  {
+    "revision": "c5ae7fbdad7c8da59ea49d17a7f0616b",
+    "url": "/box-color/index.html"
+  }
+];
